dada682bffad93529a69f28fa0bba095a8758139
AbdulMalik <solely.abdul@gmail.com>
2023-08-22 22:27:28 +0100

    Portfolio update 2.0

---
+++ README.md
@@ -0,0 +1,13 @@
+# Portfolio Site Update 2.0
+
+## Pushing new portfolio site updates with Git to GitHub
+
+## Steps
+
+1. Initialize Git - git init
+
+2. Update code
+
+3. Add 0r stage code for commit
+
+4. Commit Code
---
+++ index.js
@@ -0,0 +1,15 @@
+// Toggle Functionality
+
+const  navToggle = document.querySelector(".nav-toggle")
+const navLinks = document.querySelectorAll(".nav__link")
+
+navToggle.addEventListener("click", function() {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
